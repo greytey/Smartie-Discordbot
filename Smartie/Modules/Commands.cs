a5ea55f9a556ac270681f44759866cfa44827daa
Grey <melanie.voellmin@gmail.com>
2023-04-03 14:35:12 +0200

    initial

---
+++ Smartie/Modules/Commands.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Commands;
+using Discord;
+
+namespace Smartie.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("pong");
+        }
+    }
+}
