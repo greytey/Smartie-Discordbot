b032f86cbfb8d893abdcd61677958d2a0a3f4cf0
Joel Jütte <j.juette.inf20@stud.bbbaden.ch>
2023-04-19 14:06:19 +0200

    Pull to give melanie access

---
+++ Smartie/commands/ChatGPT.cs
@@ -0,0 +1,40 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using OpenAI_API;
+using OpenAI_API.Chat;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.Entities;
+
+namespace Smartie.commands
+{
+    public class ChatGPT : BaseCommandModule
+    {
+        [Command("chatgpt")]
+        public async Task askChatGPT(CommandContext ctx, params string[] userinput)
+        {
+            string userinputString = string.Join(' ', userinput);
+
+            OpenAIAPI openAiApi = new OpenAIAPI("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");
+
+            var chat = openAiApi.Chat.CreateConversation();
+            chat.AppendSystemMessage("Type in a query");
+
+            chat.AppendUserInput(userinputString);
+
+            string response = await chat.GetResponseFromChatbotAsync();
+
+            var outputEmbeded = new DiscordEmbedBuilder()
+            {
+                Title = "Antwort auf die Frage: " + userinputString,
+                Description = response,
+                Color = DiscordColor.Green,
+            };
+
+            await ctx.Channel.SendMessageAsync(embed: outputEmbeded);
+        }
+    }
+}
