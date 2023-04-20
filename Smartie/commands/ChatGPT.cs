7957300ae0c952675df78e7a0cf981e5ff4d8b9b
Joel Jütte <j.juette.inf20@stud.bbbaden.ch>
2023-04-20 18:40:57 +0200

    Embeded help messages, loading message for chatgpt ask command and also an error message

--- Smartie/commands/ChatGPT.cs
+++ Smartie/commands/ChatGPT.cs
@@ -17,34 +17,58 @@ namespace Smartie.commands
         [Command("ask")]
         public async Task askChatGPT(CommandContext ctx, params string[] userinput)
         {
-            string userinputString = string.Join(' ', userinput);
-
-            OpenAIAPI openAiApi = new OpenAIAPI("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");
-
-            var chat = openAiApi.Chat.CreateConversation();
-            chat.AppendSystemMessage("Type in a query");
-
-            chat.AppendUserInput(userinputString);
-
-            string response = await chat.GetResponseFromChatbotAsync();
-           
-
-            var outputEmbeded = new DiscordEmbedBuilder()
+            DiscordEmbedBuilder embededMessage = new DiscordEmbedBuilder
             {
-                Title = "Antwort auf die Frage: " + userinputString,
-                Description = response,
+                Title = "Loading...",
+                Description = "Waiting for the answer from chatgpt",
                 Color = DiscordColor.Green,
             };
+            var message = await ctx.Channel.SendMessageAsync(embed: embededMessage);
+            try
+            {
+                string userinputString = string.Join(' ', userinput);
 
-            await ctx.Channel.SendMessageAsync(embed: outputEmbeded);
+                OpenAIAPI openAiApi = new OpenAIAPI("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");
+
+                var chat = openAiApi.Chat.CreateConversation();
+                chat.AppendSystemMessage("Type in a query");
+
+                chat.AppendUserInput(userinputString);
+
+                string response = await chat.GetResponseFromChatbotAsync();
+
+                embededMessage = new DiscordEmbedBuilder()
+                {
+                    Title = "Answer to the question: " + userinputString,
+                    Description = response,
+                    Color = DiscordColor.Green,
+                };
+            }
+            catch (Exception e)
+            {
+                embededMessage = new DiscordEmbedBuilder()
+                {
+                    Title = "Error",
+                    Description = "It seems like something went wrong, please try again.",
+                    Color = DiscordColor.Red,
+                };
+            }
+            await message.DeleteAsync();
+            await ctx.Channel.SendMessageAsync(embed: embededMessage);
         }
 
         [Command("help")]
         public async Task help(CommandContext ctx)
         {
-            await ctx.Channel.SendMessageAsync("chatgpt has the following command:\n" +
+            var embededMessage = new DiscordEmbedBuilder
+            {
+                Title = "ChatGPT help",
+                Description = "chatgpt has the following command:\n" +
                 "- chatgpt ask <what you want to ask> : asks chatgpt whatever you typed" +
-                "\n\n**Remember**: Commands should always start with 'Hey Smartie '");
+                "\n\n**Remember**: Commands should always start with 'Hey Smartie '",
+                Color = DiscordColor.Blue,
+            };
+            await ctx.Channel.SendMessageAsync(embed: embededMessage);
         }
     }
 }
