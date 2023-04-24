6310c88852fc203771d6fa8138db8b40ce37ad83
Noel <n.keller.inf20@stud.bbbaden.ch>
2023-04-24 16:26:41 +0200

    Klasse für sunriseset bot erstellt und help command erstellt

---
+++ Smartie/commands/SunriseSet.cs
@@ -0,0 +1,37 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using OpenAI_API;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.Entities;
+using OpenAI_API.Completions;
+
+namespace Smartie.commands
+{
+    [Group("sunriseset")]
+    public class SunriseSet : BaseCommandModule
+    {
+        [Command("sunriseset")]
+        public async Task TestCommand(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Hello");
+        }
+
+        [Command("help")]
+        public async Task help(CommandContext ctx)
+        {
+            DiscordEmbedBuilder embededMessage = new DiscordEmbedBuilder()
+            {
+                Title = "SunriseSet help",
+                Description = "SunriseSet has the following commands:\n" +
+                "- sunriseset ask\\ bot joines the channel the user is currently in\n" +
+                "\n\n**Remember**: Commands should always start with 'Hey Smartie '",
+                Color = DiscordColor.Blue
+            };
+            await ctx.Channel.SendMessageAsync(embed: embededMessage);
+        }
+    }
+}
